d9b360848e9373f3b9ec451e2ac7e834c219c682
Ya'Rab Al-Mamari <115209826+JuniorPrice@users.noreply.github.com>
2026-06-22 10:11:20 +0400

    initialize project

---
+++ src/main/java/com/example/Clinic_Appointment_and_Patient_Records_API/ClinicAppointmentAndPatientRecordsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Clinic_Appointment_and_Patient_Records_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicAppointmentAndPatientRecordsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicAppointmentAndPatientRecordsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Clinic_Appointment_and_Patient_Records_API/ClinicAppointmentAndPatientRecordsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Clinic_Appointment_and_Patient_Records_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicAppointmentAndPatientRecordsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
